3b40537f84714cd301c668df91a17828c5fa0b16
Laurie Galler <lgaller@wgu.com>
2025-08-09 10:07:16 -0400

    Added TO DOs to model.py

--- ml/model.py
+++ ml/model.py
@@ -1,10 +1,14 @@
 import pickle
+# TODO: add necessary import
+from typing import Tuple, Any
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+
 
 # Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
+def train_model(X_train: np.ndarray, y_train: np.ndarray) -> Any:
     """
     Trains a machine learning model and returns it.
 
@@ -20,10 +24,17 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
+    model = LogisticRegression(
+        max_iter=1000,
+        solver="liblinear",
+        random_state=42,
+    )
+    model.fit(X_train, y_train)
+    return model
     pass
 
 
-def compute_model_metrics(y, preds):
+def compute_model_metrics(y: np.ndarray, preds: np.ndarray)-> Tuple[float, float, float]:
     """
     Validates the trained machine learning model using precision, recall, and F1.
 
@@ -45,7 +56,7 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
-def inference(model, X):
+def inference(model: Any, X: np.ndarray) -> np.ndarray:
     """ Run model inferences and return the predictions.
 
     Inputs
@@ -60,9 +71,10 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
+    return model.predict(X)
     pass
 
-def save_model(model, path):
+def save_model(model: Any, path: str) -> None:
     """ Serializes model to a file.
 
     Inputs
@@ -73,16 +85,20 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
     pass
 
-def load_model(path):
+def load_model(path: str) -> Any:
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
+    with open(path, "rb") as f:
+        return pickle.load(f)
     pass
 
 
 def performance_on_categorical_slice(
-    data, column_name, slice_value, categorical_features, label, encoder, lb, model
+    data, column_name: str, slice_value, categorical_features, label: str, encoder, lb, model: Any,
 ):
     """ Computes the model metrics on a slice of the data specified by a column name and
 
@@ -117,12 +133,23 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
+    df_slice = data[data[column_name] == slice_value]
+    if df_slice.empty
+        return 0.0, 0.0, 0.0
     # TODO: implement the function
-    X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+
+    X_slice, y_slice, _, _ = process_data(
+        df_slice.copy(),
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
